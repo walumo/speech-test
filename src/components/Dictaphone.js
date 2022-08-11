3b648da0dd53cfd40ed48bfd93375cb9392a4215
walumo <miikka.hakulinen@awacademy.training>
2022-08-11 15:12:21 +0300

    init

---
+++ src/components/Dictaphone.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+const Dictaphone = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+        <p>Microphone: {listening ? 'on' : 'off'}</p>
+            <button 
+                onMouseDown={SpeechRecognition.startListening} 
+                onMouseUp={SpeechRecognition.stopListening}>Speak
+            </button>
+      
+      <button onClick={resetTranscript} on>Reset</button>
+      <p className='purple'>{transcript}</p>
+    </div>
+  );
+};
+export default Dictaphone;
